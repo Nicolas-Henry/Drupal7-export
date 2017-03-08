5b31277b1cf827facf57417d631e3d385af474f3
root <root@vps304094.ovh.net>
2017-03-08 19:51:14 +0100

    Modification : database schema

--- inc/settings.php
+++ inc/settings.php
@@ -2,15 +2,16 @@
 // databases connexions
 
 // drupal 7 db
+// IMPORTANT : specify if possible a copy of our drupal 7 database
 $settings_db_drupal7['server']      = 'localhost';
-$settings_db_drupal7['user']        = 'nicolas';
-$settings_db_drupal7['password']    = 'xxxxxx';
-$settings_db_drupal7['database']    = 'drupal7-copy'; // IMPORTANT : specify if possible a copy of our drupal 7 database
+$settings_db_drupal7['user']        = '';
+$settings_db_drupal7['password']    = '';
+$settings_db_drupal7['database']    = 'drupal7-copy';
 
 // destination db
 $settings_db_destination['server']      = 'localhost';
-$settings_db_destination['user']        = 'nicolas';
-$settings_db_destination['password']    = 'yyyyyyy';
+$settings_db_destination['user']        = '';
+$settings_db_destination['password']    = '';
 $settings_db_destination['database']    = 'db-destination';
 
 // tables
@@ -19,7 +20,7 @@ $tables['2'] = 'articles';
 $tables['3'] = 'categories';
 
 // category vid = The taxonomy_vocabulary.vid of the vocabulary to which the term is assigned.
-$category_vid = 2; // 
+$category_vid = 2; 
 
 // tables structures
 $tables_create_sql['1'] = "CREATE TABLE `users` (
@@ -29,10 +30,10 @@ $tables_create_sql['1'] = "CREATE TABLE `users` (
   `mail` varchar(128) NOT NULL,
   `role` varchar(20) DEFAULT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '0',
-  `lastlogin` date NOT NULL,
+  `lastlogin` datetime DEFAULT NULL,
   `numberslogin` smallint(6) NOT NULL DEFAULT '0',
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
 
@@ -45,11 +46,11 @@ $tables_create_sql['2'] = "CREATE TABLE articles (
   `url` varchar(250) DEFAULT NULL,
   `content` text NOT NULL,
   `imagefilename` varchar(25) DEFAULT NULL,
-  `imagealt` varchar(64) NOT NULL,
-  `views` int(6) NOT NULL,
+  `imagealt` varchar(64) DEFAULT NULL,
+  `views` int(6) NOT NULL DEFAULT '0',
   `online` tinyint(1) NOT NULL DEFAULT '0',
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
 
@@ -60,7 +61,7 @@ $tables_create_sql['3'] = "CREATE TABLE `categories` (
   `name` varchar(70) NOT NULL,
   `slug` varchar(128) NOT NULL,
   `description` tinytext NOT NULL,
-  `imagefilename` varchar(64) NOT NULL,
+  `imagefilename` varchar(64) DEFAULT NULL,
   `order` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
